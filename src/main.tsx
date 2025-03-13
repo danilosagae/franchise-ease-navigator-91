6bc73e5f10c913a46ddd27dce2ff3b6bb5f7f1ca
danilosagae <danilo.sagae@vulkanbr.com>
2025-03-12 21:57:59 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,17 @@ import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+import RDStationForm from "@/components/RDStationForm";
+
+function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <h2 className="text-xl font-bold mb-4">Preencha o formulário</h2>
+      <RDStationForm />
+    </div>
+  );
+}
+
+export default App;
+
